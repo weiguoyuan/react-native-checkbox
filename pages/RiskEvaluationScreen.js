e6a7770d52e96fd9e705d438b95fe508bc8ad730
weiguoyuan@163.com <199211weiguoyuan>
2017-09-05 17:51:52 +0800

    init

---
+++ pages/RiskEvaluationScreen.js
@@ -0,0 +1,215 @@
+import React, {Component} from 'react';
+import {
+    Slider,
+    ListView,
+    Text,
+    ScrollView,
+    View,
+    TouchableOpacity,
+    Dimensions,
+    StyleSheet,
+    Alert,
+    Platform,
+    Image,
+    TextInput,
+    AsyncStorage
+} from "react-native";
+import Commons, {ProgressView, Link, context, HocNavScreen} from "commons";
+import {Navigation} from "react-native-navigation";
+import {RiskQuestionnNaireModel,RiskEvaluationResultModel} from "app-js-sdk";
+import RiskCheckBox from '../../widget/RiskCheckBox';
+import RiskRadio from '../../widget/RiskRadio';
+
+const answers = [];
+type answer={
+    stemId:Number,
+    option:[]
+}
+class RiskEvaluationScreen extends Component {
+
+    commons: Commons = new Commons().bind(this);
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            remind: true,
+            paper: null,
+            loading: false,
+        };
+        this.commons.apis.riskEvaluationApi.selectQuestionNaire().then((p: RiskQuestionnNaireModel[])=> {
+            this.setState({
+                paper: p
+            });
+            for (let i = 0; i < p.length; i++) {
+                answers[i] = {stemId: p[i].id, option: []};
+            }
+        }).default();
+    }
+
+    static navigatorStyle = {
+        navBarHidden: true,
+        tabBarHidden: true,
+    };
+
+    tag = false;
+
+    onChange = () => {
+        if(this.tag){
+            return;
+        }else{
+            this.tag=true;
+        }
+
+        for(let i=0;i<answers.length;i++){
+            if(answers[i].option.length==0){
+                Alert.alert("第"+(i+1)+"题未作答,强选择答案!");
+                return false;
+                this.tag=false;
+            }
+        }
+        let str = JSON.stringify(answers).toString();
+        this.commons.apis.riskEvaluationApi.saveRiskEvaluation(str).then((p:String)=>{
+            this.commons.apis.riskEvaluationApi.riskEvaluationResult().then((m:RiskEvaluationResultModel)=>{
+                this.props.navigator.push({
+                    screen: "RiskEvaluation.RiskEvaluationFinishScreen",
+                    passProps: {userRiskLevel:m,isTest:1},
+                });
+            }).default();
+        }).default(()=>{
+            this.tag = false;
+        })
+    };
+    callBack = (type, stemId, id) => {
+        for (let i = 0; i < answers.length; i++) {
+            if (answers[i].stemId == stemId) {
+                if (type == 1) {//多选
+                    if (answers[i].option.includes(id)) {
+                        answers[i].option.splice(answers[i].option.findIndex(id), 1);
+                    } else {
+                        answers[i].option.push(id);
+                    }
+                } else {
+                    answers[i].option[0] = id;
+                }
+            }
+        }
+    }
+
+    render() {
+
+        const {height, width} = Dimensions.get('window');
+        let mg = (width - 214) / 2;
+        if (this.state.paper == null) {
+            return <ProgressView isVisible={this.state.loading}/>;
+        }
+        let paper = this.state.paper;
+        return (
+            <ScrollView style={styles.wrap}>
+                <View style={styles.header}>
+                    <Text style={styles.h_text}>尊敬的投资者： 以下10个问题将根据您的财务状况、投资经验、投资风格、风险偏好和风险承受能力等对您进行风险评估（每个问题请选择唯一选项，不可多选），它可协助我们评估您的投资偏好和风险承受能力，有助于您控制投资的风险，同时也便于据此为您提供更准确的服务。</Text>
+                </View>
+                {paper.map((rowData, i) => (
+                    <View>
+                        <View style={styles.content}>
+                            {(()=> {
+                                if (rowData.type == 1) {
+                                    return <RiskRadio data={rowData} key={i} number={i+1}
+                                                      callBack={this.callBack}></RiskRadio>
+                                } else return <RiskCheckBox data={rowData} key={i} number={i+1}
+                                                            callBack={this.callBack}></RiskCheckBox>
+                            })()}
+                        </View>
+                        <View style={styles.k9}></View>
+                    </View>
+
+                ))}
+                <View>
+                </View>
+                <View style={styles.button}>
+                    <TouchableOpacity style={styles.btnWrap} onPress={this.onChange}>
+                        <Text style={styles.btnText}>完成</Text>
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
+        );
+    }
+
+}
+export default HocNavScreen(RiskEvaluationScreen);
+const styles = StyleSheet.create({
+    wrap: {
+        backgroundColor: '#f0f0f0',
+        flex: 1,
+    },
+    header: {
+        paddingLeft: 15,
+        paddingRight: 15,
+        paddingTop: 6,
+        paddingBottom: 6
+    },
+    h_text: {
+        fontSize: 15,
+        lineHeight: 30,
+        color: '#333',
+    },
+    content: {
+        backgroundColor: "#fff",
+    },
+    question: {
+        flexDirection: 'row',
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 15,
+        marginRight: 15
+    },
+    check: {
+        width: 20,
+        marginLeft: 15,
+        marginRight: 15
+    },
+    item: {
+        flex: 1,
+    },
+    i_text: {
+        fontSize: 15,
+        color: '#333',
+    },
+    q_title: {
+        fontSize: 15,
+        color: '#333',
+        lineHeight: 50,
+        paddingLeft: 15,
+        paddingRight: 15,
+    },
+    button: {},
+    btnWrap: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#ff7011',
+        borderTopLeftRadius: 40,
+        borderTopRightRadius: 40,
+        borderBottomLeftRadius: 40,
+        borderBottomRightRadius: 40,
+        marginTop: 20,
+        marginLeft: 15,
+        marginRight: 15,
+        height: 40,
+    },
+    btnWrapDisabled: {
+        backgroundColor: '#cdcdcd',
+    },
+    btnText: {
+        fontSize: 18,
+        color: '#fff',
+    },
+    cut: {
+        height: 1,
+        backgroundColor: '#dedede',
+        marginLeft: 15,
+        marginRight: 15,
+    },
+    k9: {
+        height: 9
+    },
+});
---
+++ widget/RiskCheckBox.js
@@ -0,0 +1,164 @@
+import React, {Component} from "react";
+import {
+    Slider,
+    ListView,
+    Text,
+    View,
+    TouchableOpacity,
+    Dimensions,
+    StyleSheet,
+    Alert,
+    Platform,
+    Image
+} from "react-native";
+import Commons, {ProgressView, Link, context, expectedRevenue, RefreshListView} from "commons";
+
+type Props = {
+    data: Object,
+    callBack:Object,
+    navigator:Object,
+    number:Number,
+};
+
+const RadioImage = require('../../img/pop/radio.png');
+const CheckRadioImage = require('../../img/pop/check_radio.png');
+
+export default class RiskCheckBox extends Component {
+    props: Props;
+    static defaultProps = {
+        data: {},
+    };
+
+    commons: Commons = new Commons().bind(this);
+    constructor(props) {
+        super(props);
+        this.state = {
+            remind: true,
+            paper:null,
+            loading: false,
+            checkedOption:[],
+        };
+        this.props.data.riskEvaluationOptionModel.map((rowData,i)=>(
+            this.setState({
+               checkedOption:this.state.checkedOption.push(0),
+            })
+        ))
+    };
+
+    check = (option,id,stemId,i)=> {
+        let checkedTemp = this.state.checkedOption;
+        checkedTemp[i] = checkedTemp[i] == 0 ? 1 : 0;
+        this.setState({
+            checkedOption:checkedTemp,
+        });
+        this.props.callBack(1,stemId,id);
+    };
+
+    render() {
+        /*const {height, width} = Dimensions.get('window');
+         let dd = width*0.2
+         let props = this.props;
+         let imageUrl = imageUri(this.props.data.imagesPathWx);*/
+        return (
+            <View>
+                <Text style={styles.q_title}>{this.props.number}.{this.props.data.title}</Text>
+                <View style={styles.cut}></View>
+                {this.props.data.riskEvaluationOptionModel.map((rowData,i) => (
+                    <View>
+                        <View style={styles.question}>
+                            <TouchableOpacity onPress={()=>{this.check(rowData.option,rowData.id,rowData.stemId,i)}}>
+                                {(()=> {
+                                    if(this.state.checkedOption[i]==1)return <Image style={styles.check} source={CheckRadioImage}/>
+                                    else return <Image style={styles.check} source={RadioImage}/>
+                                })()}
+                            </TouchableOpacity>
+                            <View style={styles.item}>
+                                <Text style={styles.i_text}>{rowData.content}</Text>
+                            </View>
+                        </View>
+                        {(()=> {
+                            if(this.state.checkedOption.length-1>i)return <View style={styles.cut}></View>
+                        })()}
+                    </View>))}
+            </View>
+        )
+    };
+};
+
+const styles = StyleSheet.create({
+    wrap: {
+        backgroundColor: '#f0f0f0',
+        flex: 1,
+    },
+    header: {
+        paddingLeft: 15,
+        paddingRight: 15,
+        paddingTop: 6,
+        paddingBottom: 6
+    },
+    h_text: {
+        fontSize: 15,
+        lineHeight: 30,
+        color: '#333',
+    },
+    content: {
+        backgroundColor: "#fff",
+    },
+    question: {
+        flexDirection: 'row',
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 15,
+        marginRight: 15
+    },
+    check: {
+        width: 20,
+        marginLeft: 15,
+        marginRight: 15
+    },
+    item: {
+        flex: 1,
+    },
+    i_text: {
+        fontSize: 15,
+        color: '#333',
+    },
+    q_title: {
+        fontSize: 15,
+        color: '#333',
+        lineHeight: 50,
+        paddingLeft: 15,
+        paddingRight: 15,
+    },
+    button: {},
+    btnWrap: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#ff7011',
+        borderTopLeftRadius: 40,
+        borderTopRightRadius: 40,
+        borderBottomLeftRadius: 40,
+        borderBottomRightRadius: 40,
+        marginTop: 20,
+        marginLeft: 15,
+        marginRight: 15,
+        height: 40,
+    },
+    btnWrapDisabled: {
+        backgroundColor: '#cdcdcd',
+    },
+    btnText: {
+        fontSize: 18,
+        color: '#fff',
+    },
+    cut: {
+        height: 1,
+        backgroundColor: '#dedede',
+        marginLeft: 15,
+        marginRight: 15,
+    },
+    k9: {
+        height: 9
+    },
+});
